97b5d88fb87a375667a9ba2fe2453c295f47b74b
tylerelk <144300250+tylerelk@users.noreply.github.com>
2024-03-30 14:52:50 -0700

    add error handling for API calls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 //imports
-import Forecast from "./modules/forecast";
 import DOMRender from "./modules/dom";
 import InitialLocation from "./modules/initiallocation";
-import Card from "./modules/card";
 
 //variables
 const weatherAPIKey = 'b58c63519071410d91613011243003';
@@ -10,32 +8,9 @@ const root = document.querySelector('.root-div');
 
 async function render() {
     //Initialize site
-    const location = await InitialLocation();
-    const weather = await Forecast(`${location.latitude},${location.longitude}`, weatherAPIKey);
-    console.log(weather);
-    DOMRender();
-    //testing card construction
-    let unit = 'farenheit';
-    let weatherReport = new Card(weather.now, weather.forecast, unit);
-    let forecast = weatherReport.renderFuture();
-    root.appendChild(weatherReport.renderToday());
-    forecast.forEach(day => root.appendChild(day));
-
-    //Change location/units
-    const locationSubmit = document.querySelector('.submit-btn');
-    locationSubmit.addEventListener('click', async (e) => {
-        e.preventDefault();
-        let location = document.querySelector('#location-input').value;
-        console.log(await Forecast(location, weatherAPIKey));
-    });
-    const tempSelect = document.querySelectorAll('.temp-switch');
-    tempSelect.forEach(option => {
-        option.addEventListener('click', (e) => {
-            unit = e.target.value;
-            root.innerHTML = '';
-            root.appendChild(weatherReport.renderToday());
-        })
-    })
+    const initialLocation = await InitialLocation();
+    let unit = 'celsius';
+    DOMRender(`${initialLocation.latitude},${initialLocation.longitude}`, unit);
 };
 
 render();
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,51 +1,73 @@
-export default function DOMRender() {
-    const rootDiv = document.querySelector('.root-div');
-    const inputArea = document.createElement('div');
-    const inputForm = document.createElement('form');
-    const locationLabel = document.createElement('label');
-    const locationInput = document.createElement('input');
-    const locationSubmit = document.createElement('input');
-    const tempSwitch = document.createElement('form');
-    const celsLabel = document.createElement('label');
-    const celsSwitch = document.createElement('input');
-    const farenLabel = document.createElement('label');
-    const farenSwitch = document.createElement('input');
+import Card from "./card";
+import Forecast from "./forecast";
 
-    locationLabel.setAttribute('for', 'location-input');
-    locationInput.setAttribute('id', 'location-input');
-    locationInput.setAttribute('type', 'text');
-    locationInput.setAttribute('placeholder', 'Enter City, Zip, etc.');
-    locationSubmit.setAttribute('type', 'submit');
-    locationSubmit.setAttribute('value', 'Search');
-    locationSubmit.classList.add('submit-btn');
-    locationSubmit.classList.add('btn');
-    tempSwitch.setAttribute('id', 'temp-swtch');
-    celsSwitch.setAttribute('type', 'radio');
-    celsSwitch.setAttribute('name', 'temp-select');
-    celsSwitch.setAttribute('id', 'celsius-switch');
-    celsSwitch.setAttribute('value', 'celsius');
-    celsSwitch.classList.add('temp-switch');
-    farenSwitch.setAttribute('type', 'radio');
-    farenSwitch.setAttribute('name', 'temp-select');
-    farenSwitch.setAttribute('id', 'farenheit-switch');
-    farenSwitch.setAttribute('value', 'farenheit');
-    farenSwitch.classList.add('temp-switch');
+export default async function DOMRender(newLocation, units) {
+  const APIkey = "b58c63519071410d91613011243003";
+  const weather = await Forecast(newLocation, APIkey);
+  const root = document.querySelector(".root-div");
 
-    inputForm.appendChild(locationLabel);
-    inputForm.appendChild(locationInput);
-    inputForm.appendChild(locationSubmit);
+  root.innerHTML = "";
 
-    tempSwitch.appendChild(celsLabel);
-    tempSwitch.appendChild(celsSwitch);
-    tempSwitch.appendChild(farenLabel);
-    tempSwitch.appendChild(farenSwitch);
+  const weatherDisplay = new Card(weather.now, weather.forecast, units);
+  const weatherDisplayFuture = weatherDisplay.renderFuture();
 
-    locationLabel.textContent = 'Change Location:';
-    celsLabel.textContent = 'C°';
-    farenLabel.textContent = 'F°';
+  const currentLocation = document.createElement("h2");
+  const inputArea = document.createElement("div");
+  const inputForm = document.createElement("div");
+  const locationLabel = document.createElement("label");
+  const locationInput = document.createElement("input");
+  const locationSubmit = document.createElement("button");
+  const tempSwitch = document.createElement("div");
+  const celsSwitch = document.createElement("button");
+  const farenSwitch = document.createElement("button");
 
-    inputArea.appendChild(inputForm);
-    inputArea.appendChild(tempSwitch);
+  locationLabel.setAttribute("for", "location-input");
+  locationInput.setAttribute("id", "location-input");
+  locationInput.setAttribute("type", "text");
+  locationInput.setAttribute("placeholder", "Enter City, Zip, etc.");
+  locationSubmit.setAttribute("value", "Search");
+  locationSubmit.classList.add("submit-btn");
+  locationSubmit.classList.add("btn");
+  tempSwitch.setAttribute("id", "temp-swtch");
+  celsSwitch.setAttribute("id", "celsius-switch");
+  celsSwitch.setAttribute("value", "celsius");
+  celsSwitch.classList.add("temp-switch");
+  farenSwitch.setAttribute("id", "farenheit-switch");
+  farenSwitch.setAttribute("value", "farenheit");
+  farenSwitch.classList.add("temp-switch");
 
-    rootDiv.appendChild(inputArea);
+  locationSubmit.addEventListener("click", () => {
+    if (!locationInput.value) {
+      locationInput.setAttribute("placeholder", "Enter a location!");
+    } else {
+      DOMRender(locationInput.value, units);
+    }
+  });
+
+  inputForm.appendChild(locationLabel);
+  inputForm.appendChild(locationInput);
+  inputForm.appendChild(locationSubmit);
+
+  tempSwitch.appendChild(celsSwitch);
+  tempSwitch.appendChild(farenSwitch);
+
+  tempSwitch.childNodes.forEach((node) => {
+    node.addEventListener("click", (e) => {
+      DOMRender(newLocation, e.target.value);
+    });
+  });
+
+  currentLocation.textContent = `${weather.location.city}, ${weather.location.country}`;
+  locationLabel.textContent = "Change Location:";
+  celsSwitch.textContent = "C°";
+  farenSwitch.textContent = "F°";
+  locationSubmit.textContent = "Search";
+
+  inputArea.appendChild(inputForm);
+  inputArea.appendChild(tempSwitch);
+
+  root.appendChild(inputArea);
+  root.appendChild(currentLocation);
+  root.appendChild(weatherDisplay.renderToday());
+  weatherDisplayFuture.forEach((day) => root.appendChild(day));
 }
