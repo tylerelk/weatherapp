d8720e4b64cb51d2947c9681e10cf5fdb5afd85c
tylerelk <144300250+tylerelk@users.noreply.github.com>
2024-03-29 20:30:48 -0700

    configure API request, format data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Works');
+//imports
+import Forecast from "./modules/forecast";
+
+//variables
+const root = document.querySelector('.root-div');
+const weatherAPIKey = 'b58c63519071410d91613011243003';
+
+async function render() {
+    console.log(await Forecast('sydney', weatherAPIKey));
+};
+
+render();
---
+++ src/modules/forecast.js
@@ -0,0 +1,41 @@
+export default async function Forecast(location, key) {
+    const weatherAPI = 'http://api.weatherapi.com/v1/forecast.json';
+    let result;
+    let weather = {
+        location: {},
+        now: {},
+        forecast: []
+    };
+    
+    async function getWeather(location, key) {
+        await fetch(`${weatherAPI}?key=${key}&q=${location}&days=3`)
+        .then(result => result.json())
+        .then(data => result = data);
+    };
+    await getWeather(location, key);
+    
+    weather.location.country = result.location.country;
+    weather.location.city = result.location.name;
+    weather.location.time = result.location.localtime;
+    weather.now.celsius = result.current['temp_c'];
+    weather.now.farenheit = result.current['temp_f'];
+    weather.now.icon = result.current.condition.icon;
+    weather.now.condition = result.current.condition.text;
+    formatDays(result.forecast.forecastday);
+
+    function formatDays(arr) {
+        arr.forEach(day => {
+            weather.forecast.push({
+                date: day.date,
+                maxCelsius: day.day.maxtemp_c,
+                minCelsius: day.day.mintemp_c,
+                maxFarenheit: day.day.maxtemp_f,
+                minFarenheit: day.day.mintemp_f,
+                condition: day.day.condition.text,
+                icon: day.day.condition.icon
+            })
+        })
+    }
+    
+    return weather;
+}
